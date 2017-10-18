ee8653eba9fab249a0590694df7e32f72275a6dc
Talin <tjw.tech@gmail.com>
2017-10-17 19:04:59 -0700

    Made pep8 compliant

--- count_keywords.py
+++ count_keywords.py
@@ -1,9 +1,32 @@
 import requests
-import bs4 
+import bs4
 import pandas as pd
 from list_jobs import get_all_parameters_for_all_listings
 
-# Build search_query and get dataframe containing all associated links
+
+def get_text(links):
+    text_by_page = []
+
+    for link in links:
+        if 'http' in link:
+            url = link
+        else:
+            url = 'https://' + link
+        print(url)
+
+        response = requests.get(url)
+        print(response.status_code)
+
+        html = response.text
+        soup = bs4.BeautifulSoup(html, 'html.parser')
+
+        page_text = soup.findAll(text=True)
+        text_by_page.append(page_text)
+
+    return(text_by_page)
+
+
+# Build search_query and get a dataframe containing all associated links
 search_keyword = 'firefighter'
 search_location = 'Bay Area, CA'
 search_query = 'jobs?q=' + search_keyword + '&l=' + search_location
@@ -12,19 +35,10 @@ print(search_url)
 
 df_all_parameters = get_all_parameters_for_all_listings(search_url)
 
-# Store all links associated with search_query in a list; loop through that list
+# Get the text associated with every link
 all_links = df_all_parameters['Link'].tolist()
- 
-for link in all_links:
-    current_url = 'https://' + link
+all_text = get_text(all_links)
 
-    response = requests.get(current_url)
-    print(response.status_code)
-    
-    html = response.text
-    soup = bs4.BeautifulSoup(html, 'html.parser')
-
-    # Get all the text from a given page
-    page_text = soup.findAll(text=True)
-    print(page_text)
-    
+# Loop through every word on every page:
+# If unique: add it to a dictionary
+# Else: increment word counter
--- list_jobs.py
+++ list_jobs.py
@@ -3,19 +3,22 @@ import bs4
 import pandas as pd
 from datetime import datetime
 
+
 def get_jobs(soup):
     jobs = []
-    inline_elements = soup.find_all(name='a', attrs={'data-tn-element':'jobTitle'})
-    for element in inline_elements:
+    elements = soup.find_all(name='a', attrs={'data-tn-element': 'jobTitle'})
+    for element in elements:
             jobs.append(element['title'])
     return(jobs)
 
-def get_companies(soup): 
+
+def get_companies(soup):
     companies = []
-    spans = soup.find_all(name='span', attrs={'class':'company'})
+    spans = soup.find_all(name='span', attrs={'class': 'company'})
     for span in spans:
         companies.append(span.text)
     return(companies)
+
 
 def get_locations(soup):
     locations = []
@@ -24,6 +27,7 @@ def get_locations(soup):
         locations.append(span.text)
     return(locations)
 
+
 def get_summaries(soup):
     summaries = []
     spans = soup.findAll('span', attrs={'class': 'summary'})
@@ -31,26 +35,29 @@ def get_summaries(soup):
         summaries.append(span.text)
     return(summaries)
 
+
 def get_ages(soup):
     ages = []
-    divs = soup.find_all(name='div', attrs={'class':'result-link-bar'})
+    divs = soup.find_all(name='div', attrs={'class': 'result-link-bar'})
     for div in divs:
-        span = div.find(name='span', attrs={'class':'date'})
-        if span == None:
-            ages.append("No age found")
-        else:
+        span = div.find(name='span', attrs={'class': 'date'})
+        if span is not None:
             ages.append(span.text)
+        else:
+            ages.append("No age found")
     return(ages)
+
 
 def get_links(soup):
     links = []
-    inline_elements = soup.find_all(name='a', attrs={'data-tn-element':'jobTitle'})
-    for element in inline_elements:
+    elements = soup.find_all(name='a', attrs={'data-tn-element': 'jobTitle'})
+    for element in elements:
             links.append('www.indeed.com' + str(element['href']))
     return(links)
 
+
 def does_a_nextpage_exist(soup):
-    spans = soup.find_all(name='span', attrs={'class':'np'})
+    spans = soup.find_all(name='span', attrs={'class': 'np'})
     for span in spans:
         if 'Next' in span.text:
             nextpage_exists = True
@@ -58,16 +65,18 @@ def does_a_nextpage_exist(soup):
             nextpage_exists = False
     return(nextpage_exists)
 
+
 def get_nextpage_url(soup):
-    div = soup.find(name='div', attrs={'class':'pagination'})
-    inline_elements = div.find_all(name='a')
-    nextpage_url = 'https://www.indeed.com/' + str(list(inline_elements)[-1]['href'])
+    div = soup.find(name='div', attrs={'class': 'pagination'})
+    elements = div.find_all(name='a')
+    nextpage_url = 'https://www.indeed.com/' + str(list(elements)[-1]['href'])
     return(nextpage_url)
+
 
 def get_all_parameters_for_all_listings(url):
     response = requests.get(url)
     print(response.status_code)
-    
+
     html = response.text
     soup = bs4.BeautifulSoup(html, 'html.parser')
 
@@ -77,7 +86,7 @@ def get_all_parameters_for_all_listings(url):
     all_summaries = []
     all_ages = []
     all_links = []
-    
+
     page_counter = 1
 
     while True:
@@ -89,32 +98,32 @@ def get_all_parameters_for_all_listings(url):
 
         currentpage_locations = get_locations(soup)
         all_locations.extend(currentpage_locations)
-        
+
         currentpage_summaries = get_summaries(soup)
         all_summaries.extend(currentpage_summaries)
-        
+
         currentpage_ages = get_ages(soup)
         all_ages.extend(currentpage_ages)
-        
+
         currentpage_links = get_links(soup)
         all_links.extend(currentpage_links)
 
-        # Check to see if this is the last page; if not, then navigate to the next page
+        # Check to see if this is the last page; if not, move to the next page
         nextpage_exists = does_a_nextpage_exist(soup)
         print(nextpage_exists)
-        if nextpage_exists == True:
+        if nextpage_exists is True:
             page_counter += 1
             print(page_counter)
-        
+
             nextpage_url = get_nextpage_url(soup)
             print(nextpage_url)
-        
+
             response = requests.get(nextpage_url)
             print(response.status_code)
-        
+
             html = response.text
             soup = bs4.BeautifulSoup(html, 'html.parser')
-            
+
         else:
             break
 
@@ -123,8 +132,8 @@ def get_all_parameters_for_all_listings(url):
     print('Print current # of locations: ' + str(len(all_locations)))
     print('Print current # of summaries: ' + str(len(all_summaries)))
     print('Print current # of ages: ' + str(len(all_ages)))
-    print('Print current # of links: ' + str(len(all_links)))    
-    
+    print('Print current # of links: ' + str(len(all_links)))
+
     df_all_parameters = pd.DataFrame(
         {'Job_Title': all_jobs,
          'Company_Name': all_companies,
@@ -135,6 +144,7 @@ def get_all_parameters_for_all_listings(url):
 
     return(df_all_parameters)
 
+
 search_keyword = 'firefighter'
 search_location = 'Bay Area, CA'
 search_query = 'jobs?q=' + search_keyword + '&l=' + search_location
@@ -144,8 +154,6 @@ print(search_url)
 df_all_parameters = get_all_parameters_for_all_listings(search_url)
 current_date = datetime.now()
 
-df_all_parameters.to_csv(current_date.strftime('%Y-%m-%d') + '_' + search_keyword.upper() + '.csv')
-
-
-
-
+df_all_parameters.to_csv(
+    current_date.strftime('%Y-%m-%d') + '_' + search_keyword.upper() + '.csv'
+    )
